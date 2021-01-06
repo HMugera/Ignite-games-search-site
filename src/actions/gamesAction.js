0573e3f83f67845fe65b66b7591fe0aa2bff328a
hmugera <mugerahughes@gmail.com>
2021-01-07 01:04:16 +0300

    fetching the rest of our data

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,14 +1,18 @@
 import Axios from "axios";
-import { popularGamesURL } from "../Data/api";
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from "../Data/api";
 
 //Action Creator :making an async function this wierd syntax below
 export const loadGames = () => async (dispatch) => {
 	//FETCH AXIOS
 	const popularData = await Axios.get(popularGamesURL());
+	const newGamesData = await Axios.get(newGamesURL());
+	const upcomingData = await Axios.get(upcomingGamesURL());
 	dispatch({
 		type: "FETCH_GAMES",
 		payload: {
 			popular: popularData.data.results,
+			upcoming: upcomingData.data.results,
+			newGames: newGamesData.data.results,
 		},
 	});
 };
